2d3bd64b5843aacf2b872c45260a69fa433ec3f0
hwan <dafg1878@gmail.com>
2023-11-07 13:26:46 +0900

    로그인 모달창

--- src/main/frontend/src/Component/header.js
+++ src/main/frontend/src/Component/header.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import './header.css';
+import Signin from './signin';
 import { Link } from 'react-router-dom';
 
 export default function header() {
-
+  
   return (
 <div className='header_wrap' style={{ opacity: 1 }}>
   <div className='header_category_wrap'>
@@ -58,41 +59,13 @@ export default function header() {
         <li className='header_list'>
         <a herf="#" >회사소개</a>
         </li>
+        <li className='header_list'>
+        <Signin />
+        </li>
       </ul>
     </div>
   </div>
 </div>
-
-    
-
-
-
-    // <div className='header'>
-    //   <div className='upHeader'>
-    //       <div className='logoIMG'><img className="logoImg" src="/img/ruruLogo.png" /></div>
-    //     <div className='category'>
-    //       <a href='#'>오피스텔</a>
-    //       <a href='#'>아파트/단독주택/원룸</a>
-    //       <a href='#'>상가/사무실</a>
-    //       <a href='#'>지식산업센터</a>
-    //       <a href='#'>공장창고</a>
-    //       <a href='#'>토지</a>
-    //     </div>
-    //     <div className='rightHeader'>
-    //       <span>문의전화</span>
-    //       <span>로그인</span>
-    //     </div>
-    //   </div>
-    //   <hr />
-    //   <div className='underHeader'>
-    //     <a href='#'>매물의뢰</a>
-    //     <a href='#'>부동산뉴스</a>
-    //     <a href='#'>고객후기</a>
-    //     <a href='#'>공지사항</a>
-    //     <a href='#'>찾아오시는길</a>
-    //     <a href='https://blog.naver.com/youghoa'>네이버블로그</a>
-    //   </div>
-    // </div>
   )
 }
 
