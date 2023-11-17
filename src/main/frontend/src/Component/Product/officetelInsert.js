dbf22bb5edf09839107a8a794df79caaacac5e1b
rlatngjs8 <tngjs97@gamil.com>
2023-11-17 17:20:00 +0900

    매물등록폼 완

--- src/main/frontend/src/Component/Product/officetelInsert.js
+++ src/main/frontend/src/Component/Product/officetelInsert.js
@@ -3,6 +3,7 @@ import Header from '../header.js';
 import Bottom from '../bottom.js';
 import axios from 'axios';
 import { hangjungdong } from '../Home/hangjungdong.js';
+import './officetelInsert.css';
 
 export default function OfficetelInsert() {
   const [floorExposure, setFloorExposure] = useState('노출');
@@ -17,6 +18,12 @@ export default function OfficetelInsert() {
     setMaintenanceCost(formattedValue);
   };
 
+  const [selectedFiles, setSelectedFiles] = useState([]);  // 이미지파일 첨부
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    const filesArray = Array.from(files);
+    setSelectedFiles(filesArray);
+  };
   const [val1, setVal1] = useState(""); //지역 state
   const [val2, setVal2] = useState("");
   const [val3, setVal3] = useState("");
@@ -47,6 +54,7 @@ export default function OfficetelInsert() {
       <div className='InsertDIV'>
         <h3>매물등록</h3>
         <form onSubmit={onSubmit}>
+          <h5>매물등록</h5>
           <table className='content_insert'>
             <tbody>
               <tr>
@@ -187,22 +195,162 @@ export default function OfficetelInsert() {
               </tr>
             </tbody>
           </table>
+          <h5>가격</h5>
           <table className='price_insert'>
-<tbody>
-  <tr>
-    <td>거래종류</td>
-    <td>라디오로 매매전세월세단기</td>
-  </tr>
-  <tr>
-    <td>매매가</td><td><input type='text'/>만원 (컴마찍기)</td>
-  </tr>
-  <tr>
-    <td>융자금</td><td><input type='text' />만원</td>
-  </tr>
-  <tr>
-    <td>기전세금(월세금)</td><td>전세(보종금)<input type='text'/>만원 / 월세가<input type='text'/>만원</td>
-  </tr>
-</tbody>
+            <tbody>
+              <tr>
+                <td>거래종류</td>
+                <td>라디오로 매매전세월세단기</td>
+              </tr>
+              <tr>
+                <td>매매가</td><td><input type='text' />만원 (컴마찍기)</td>
+              </tr>
+              <tr>
+                <td>융자금</td><td><input type='text' />만원</td>
+              </tr>
+              <tr>
+                <td>기전세금(월세금)</td><td>전세(보종금)<input type='text' />만원 / 월세가<input type='text' />만원</td>
+              </tr>
+            </tbody>
+          </table>
+          <h5>주차정보</h5>
+          <table>
+            <tbody>
+              <tr>
+                <td>총 주차대수</td>
+                <td><input type='text' />대 *총 세대수: ~대</td>
+                <td>세대당 주차대수</td>
+                <td><input type='text' />대</td>
+                {/* 자동계산되도록 */}
+              </tr>
+            </tbody>
+          </table>
+          <h5>시설정보</h5>
+          <table>
+            <tr>
+              <td>난방방식</td>
+              <td>
+                <select>
+                  <option>개별난방</option>
+                </select>
+              </td>
+              <td>난방연료</td>
+              <td>
+                <select>
+                  <option>도시가스</option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>냉방시설</td>
+              <td colspan="3">
+                <ul class="optionList">
+                  <li><input name="btin_cd" id="btin_cd52" value="52" type="checkbox" class="input_check" /> <label for="btin_cd52">벽걸이에어컨</label></li>
+                  <li><input name="btin_cd" id="btin_cd53" value="53" type="checkbox" class="input_check" /> <label for="btin_cd53">스탠드에어컨</label></li>
+                  <li><input name="btin_cd" id="btin_cd54" value="54" type="checkbox" class="input_check" /> <label for="btin_cd54">천장에어컨</label></li>
+
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>생활시설</td>
+              <td colSpan={3}>
+                <ul class="optionList">
+                  <li><input name="btin_cd" id="btin_cd55" value="55" type="checkbox" class="input_check" /> <label for="btin_cd55">침대</label></li>
+                  <li><input name="btin_cd" id="btin_cd56" value="56" type="checkbox" class="input_check" /> <label for="btin_cd56">책상</label></li>
+                  <li><input name="btin_cd" id="btin_cd57" value="57" type="checkbox" class="input_check" /> <label for="btin_cd57">옷장</label></li>
+                  <li><input name="btin_cd" id="btin_cd58" value="58" type="checkbox" class="input_check" /> <label for="btin_cd58">붙박이장</label></li>
+                  <li><input name="btin_cd" id="btin_cd59" value="59" type="checkbox" class="input_check" /> <label for="btin_cd59">식탁</label></li>
+                  <li><input name="btin_cd" id="btin_cd60" value="60" type="checkbox" class="input_check" /> <label for="btin_cd60">소파</label></li>
+                  <li><input name="btin_cd" id="btin_cd61" value="61" type="checkbox" class="input_check" /> <label for="btin_cd61">신발장</label></li>
+                  <li><input name="btin_cd" id="btin_cd62" value="62" type="checkbox" class="input_check" /> <label for="btin_cd62">냉장고</label></li>
+                  <li><input name="btin_cd" id="btin_cd63" value="63" type="checkbox" class="input_check" /> <label for="btin_cd63">세탁기</label></li>
+                  <li><input name="btin_cd" id="btin_cd64" value="64" type="checkbox" class="input_check" /> <label for="btin_cd64">건조기</label></li>
+                  <li><input name="btin_cd" id="btin_cd65" value="65" type="checkbox" class="input_check" /> <label for="btin_cd65">샤워부스</label></li>
+                  <li><input name="btin_cd" id="btin_cd66" value="66" type="checkbox" class="input_check" /> <label for="btin_cd66">욕조</label></li>
+                  <li><input name="btin_cd" id="btin_cd67" value="67" type="checkbox" class="input_check" /> <label for="btin_cd67">비데</label></li>
+                  <li><input name="btin_cd" id="btin_cd68" value="68" type="checkbox" class="input_check" /> <label for="btin_cd68">싱크대</label></li>
+                  <li><input name="btin_cd" id="btin_cd69" value="69" type="checkbox" class="input_check" /> <label for="btin_cd69">식기세척기</label></li>
+                  <li><input name="btin_cd" id="btin_cd70" value="70" type="checkbox" class="input_check" /> <label for="btin_cd70">가스레인지</label></li>
+                  <li><input name="btin_cd" id="btin_cd71" value="71" type="checkbox" class="input_check" /> <label for="btin_cd71">인덕션레인지</label></li>
+                  <li><input name="btin_cd" id="btin_cd72" value="72" type="checkbox" class="input_check" /> <label for="btin_cd72">전자레인지</label></li>
+                  <li><input name="btin_cd" id="btin_cd73" value="73" type="checkbox" class="input_check" /> <label for="btin_cd73">가스오븐</label></li>
+                  <li><input name="btin_cd" id="btin_cd86" value="86" type="checkbox" class="input_check" /> <label for="btin_cd86">TV</label></li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>보안시설</td>
+              <td colspan="3">
+                <ul class="optionList">
+                  <li><input name="btin_cd" id="btin_cd74" value="74" type="checkbox" class="input_check" /> <label for="btin_cd74">경비원</label></li>
+                  <li><input name="btin_cd" id="btin_cd75" value="75" type="checkbox" class="input_check" /> <label for="btin_cd75">비디오폰</label></li>
+                  <li><input name="btin_cd" id="btin_cd76" value="76" type="checkbox" class="input_check" /> <label for="btin_cd76">인터폰</label></li>
+                  <li><input name="btin_cd" id="btin_cd77" value="77" type="checkbox" class="input_check" /> <label for="btin_cd77">카드키</label></li>
+                  <li><input name="btin_cd" id="btin_cd78" value="78" type="checkbox" class="input_check" /> <label for="btin_cd78">CCTV</label></li>
+                  <li><input name="btin_cd" id="btin_cd79" value="79" type="checkbox" class="input_check" /> <label for="btin_cd79">사설경비</label></li>
+                  <li><input name="btin_cd" id="btin_cd80" value="80" type="checkbox" class="input_check" /> <label for="btin_cd80">현관보안</label></li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>기타시설</td>
+              <td colspan="3">
+                <ul class="optionList">
+                  <li><input name="btin_cd" id="btin_cd81" value="81" type="checkbox" class="input_check" /> <label for="btin_cd81">엘리베이터</label></li>
+                  <li><input name="btin_cd" id="btin_cd82" value="82" type="checkbox" class="input_check" /> <label for="btin_cd82">화재경보기</label></li>
+                  <li><input name="btin_cd" id="btin_cd85" value="85" type="checkbox" class="input_check" /> <label for="btin_cd85">무인택배함</label></li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <td>방범창/베란다</td>
+              <td colspan="3">
+                <ul class="optionList">
+                  <li><input name="btin_cd" id="btin_cd83" value="83" type="checkbox" class="input_check" /> <label for="btin_cd83">방범창</label></li>
+                  <li><input name="btin_cd" id="btin_cd84" value="84" type="checkbox" class="input_check" /> <label for="btin_cd84">베란다</label></li>
+                </ul>
+              </td>
+            </tr>
+          </table>
+          <h5>입주일</h5>
+          <table>
+            <tbody>
+              <tr>
+                <td>입주가능일</td>
+                <td>
+                  <input type='radio' /><label>즉시입주</label><br />
+                  <div><input type='radio' />날짜선택</div>
+                  <div>협의가능</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <h5>상세정보</h5>
+          <table>
+            <tbody>
+              <tr>
+                <td>매물특징</td>
+                <td><input type='text' placeholder='리스트에 노출되는 문구' /></td>
+              </tr>
+              <tr>
+                <td>상세설명</td>
+                <td>
+                  <textarea placeholder='매물 상세 페이지에 노출되는 문구입니다.'></textarea>
+                </td>
+              </tr>
+              <tr>
+                <td>매물사진</td>
+                <td>
+                  <input type='file' multiple onChange={handleFileChange} /><br />
+                  <div>
+                    {selectedFiles.map((file, index) => (
+                      <img key={index} src={URL.createObjectURL(file)} alt={`Preview ${index}`} style={{ width: '100px', height: '100px', marginRight: '10px' }} />
+                    ))}
+                  </div>
+                </td>
+              </tr>
+            </tbody>
           </table>
           <button type='submit' className='upload'>등록하기</button>
           <button type='button' className='back' onClick={() => { window.location.href = '/' }}>홈으로</button>
