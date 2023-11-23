85a23d76b0e6b4d0214111d6ab9cb1623b9f15b0
rlatngjs8 <tngjs97@gamil.com>
2023-11-23 13:55:21 +0900

    갈게..

--- src/main/java/com/real/estate/Controller/KimController.java
+++ src/main/java/com/real/estate/Controller/KimController.java
@@ -1,13 +1,10 @@
 package com.real.estate.Controller;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,6 +59,8 @@ public class KimController {
     @RequestParam String floor_open,@RequestParam String direction_criteria, @RequestParam String direction, @RequestParam String entrance, @RequestParam int rooms, @RequestParam int bathroom, @RequestParam String roomuse, @RequestParam String inner_structure,@RequestParam String administration_cost, @RequestParam String maintenance, @RequestParam String managementCost_includ,
     @RequestParam String building_dateType, @RequestParam String transactionType, @RequestParam String desiredAmount, @RequestParam String loan, @RequestParam String existingTenant_deposit, @RequestParam String existingTenant_monthlyRent, @RequestParam int total_parking, @RequestParam String parking_per_room, @RequestParam String heating_method, @RequestParam String heating_fuel,
     @RequestParam String airCondition, @RequestParam String living_facilities,@RequestParam String security_facilities, @RequestParam String other_facilities, @RequestParam String balcony, @RequestParam String moveable_date, @RequestParam String product_title, @RequestParam String product_content){
+
+        System.out.println("프로덕트 타입" + product_type);
 
         pDAO.product_insert(product_type, location, building_name, building_use, extent, address, floor, floor_open, direction_criteria, direction, entrance, rooms, bathroom, roomuse, inner_structure, administration_cost, maintenance, managementCost_includ,
                         building_dateType, transactionType, desiredAmount, loan, existingTenant_deposit, existingTenant_monthlyRent, product_content, total_parking, parking_per_room, heating_method, heating_fuel, airCondition, living_facilities, security_facilities, other_facilities, balcony, moveable_date, product_title, product_content);
