b34d30fcc57dac2da76ebb73b1555c236955bab0
gwangjin <n45202605@gmail.com>
2023-12-30 02:35:58 +0900

    회원가입 css 진행 중

--- src/main/frontend/src/Component/SingUp/SignUp.js
+++ src/main/frontend/src/Component/SingUp/SignUp.js
@@ -38,17 +38,18 @@ export default function SignUp({ isOpen, onClose }) {
 
   return (
     <div className={`modal ${isOpen ? 'open' : ''}`}>
-      <div className="modal-content">
-        <button type="button" id="closeButton" className="close" data-dismiss="modal" aria-hidden="true" onClick={close}>
+      <div className="modal-content-sign">
+        <a id="closeButton" className="signin_close" data-dismiss="modal" aria-hidden="true" onClick={close}>
             <span style={{ fontSize: '30px' }}>×</span>
-        </button>
-        <h3 className="text-center m-t-sm" style={{ marginLeft: '25px' }}>회원가입</h3>
-        <form className="form-horizontal m-t-lg" noValidate onSubmit={handleSubmit}>
+        </a>
+        <div className='login_title'>
+          <h2>회원가입</h2>
+          <span>루루부동산 방문을 환영합니다.</span>
+        </div>
+        <form className='login_form' noValidate onSubmit={handleSubmit}>
             <input type="hidden" name="member_type" value="general" />
 
-            <div className="form-group m-t">
                 <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} className="form-control input-sm" placeholder="이름" />
-            </div>
 
             <div className="form-group m-t">
                 <input type="text" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)} className="form-control input-sm" placeholder="전화번호" />
@@ -66,26 +67,17 @@ export default function SignUp({ isOpen, onClose }) {
                 <input type="password" className="form-control input-sm" id="password_re" name="signUpValue" placeholder="비밀번호 확인" />
             </div>
 
-            {/* <div className="form-group autoSign">
-                <div className="title">자동가입방지확인</div>
-                <div className="inputbox" id="inputbox">
-                    <div className="questionBox" id="questionBox">81+70 = </div>
-                    <div className="answerBox" id="answerBox"><input type="text" name="answer" id="answer" /></div>
-                </div>
-                <div id="chk_stat" style={{ display: 'none' }}></div>
-                <input type="hidden" id="sign_statement" name="sign_statement" value="N" />
-            </div> */}
+            <div className="form-group m-t text-left">
 
-            <div className="form-group text-left">
                 <div className="check">
+                    <small style={{marginRight: '150px', marginLeft: '20px'}}><a href="#modal-privacy" className="text-info">개인보호 정책</a></small>
                     <input id="terms_agree" name="terms_agree" type="checkbox" />
                     <label className="이용약관" htmlFor="terms_agree"><span>이용약관 동의</span></label>
                 </div>
-                <small><a href="#modal-privacy" className="text-info">개인보호 정책</a></small>
             </div>
 
             <div className="form-group">
-                <button type="submit" style={{ backgroundColor: 'rgb(136, 136, 136)' }}>회원가입</button>
+                <button type="submit" className='btn btn-ruru'>가입하기</button>
             </div>
         </form>
       </div>
